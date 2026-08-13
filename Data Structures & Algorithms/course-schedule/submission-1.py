ddaf3404f658773b3c5576be8e6abcc29b4c199d
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2026-08-13 10:28:00 +0200

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        state = defaultdict(int)
+
+        # Build the adj
+        # states: 0->not visited,1->in process,2->no loop
+        for a,b in prerequisites:
+            adj[b].append(a)
+
+        def dfs(course: int):
+            if state[course] == 2:
+                return True
+            if state[course] == 1:
+                return False
+            
+            state[course] = 1
+
+            for n in adj[course]:
+                if not dfs(n):
+                    return False
+            state[course] = 2
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
+        
