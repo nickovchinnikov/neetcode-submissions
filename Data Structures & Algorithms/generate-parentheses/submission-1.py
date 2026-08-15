242990edf39016d57e702494d3c973834d028d99
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2026-08-15 17:09:27 +0200

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def dfs(opened, closed, subset):
+            if opened > n or closed > n:
+                return
+
+            if len(subset) == 2*n:
+                res.append(subset)
+                return
+            
+            dfs(opened+1, closed, subset + "(")
+
+            if opened > closed:
+                dfs(opened, closed+1, subset + ")")
+            
+        dfs(0, 0, "")
+        return res
