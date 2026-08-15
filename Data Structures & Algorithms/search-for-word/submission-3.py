af11475971d9c305a2ae4691749aa4a6f09d52cf
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2026-08-15 18:17:46 +0200

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,49 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        neighboth = (
+            (1,0),
+            (-1,0),
+            (0,1),
+            (0,-1),
+        )
+
+        visited = set()
+
+        def dfs(x,y,depth):
+            if (x,y) in visited:
+                return False
+
+            if board[x][y] != word[depth]:
+                return False
+
+            if depth == len(word)-1:
+                return True
+            
+            visited.add((x,y))
+            
+            for dx,dy in neighboth:
+                xn = x+dx
+                yn = y+dy
+
+                if (
+                    xn < 0 or xn > len(board)-1 or
+                    yn < 0 or yn > len(board[x])-1
+                ):
+                    continue
+                
+                if dfs(xn,yn,depth+1):
+                    return True
+                    
+            visited.remove((x,y))
+            return False
+
+        # Find start and run
+        for x, row in enumerate(board):
+            for y, cell in enumerate(row):
+                if cell == word[0]:
+                    if dfs(x,y,0):
+                        return True
+        
+        return False
+
+
