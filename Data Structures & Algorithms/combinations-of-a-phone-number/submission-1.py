5c9427243418826699460df8725b2a1fb6afac09
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2026-08-18 15:10:01 +0200

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        
+        charmap = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz"
+        }
+
+        result = []
+        def dfs(start, subset=""):
+            if start == len(digits):
+                result.append(subset)
+                return
+
+            chars = charmap[int(digits[start])]
+            for c in chars:
+                subset += c
+                dfs(start+1, subset)
+                subset = subset[:-1]
+
+        dfs(0)
+        return result
